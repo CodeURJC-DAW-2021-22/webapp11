6cbf21e765378bbac2667c69bd1710618a51be51
amarkosmarkos <amarkosmarkos@gmail.com>
2022-06-26 22:57:27 +0200

    Pestaña de usuario terminada - Posibilidad de cambiar la contraseña - Restringir la página al usuario Cambiar nombres de variables, archivos etc. para respetar el camelCase

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import com.example.demo.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    private UserRepository repository;
+
+    public User findById(long id) {
+        return repository.findById(id).orElseThrow();
+
+    }
+
+    public User findByName(String name) {
+        return repository.findByName(name).orElseThrow();
+
+    }
+
+    public boolean exist(long id) {
+        return repository.existsById(id);
+    }
+
+    public Page<User> findAll(int n) {
+        return repository.findAll( PageRequest.of(n, 5));
+    }
+
+    public void save(User user) {
+        repository.save(user);
+    }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+
+
+
+}
